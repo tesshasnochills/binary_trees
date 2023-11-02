9addb4df98fff4942ac7fdb8a38827f8f91ebc23
tesshasnochills <tess.nganga2020@gmail.com>
2023-11-02 15:32:10 +0300

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - a function to print the sibling of a node
+ * @node: the node to print the sibling to
+ *
+ * Return: the sibling to the node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
